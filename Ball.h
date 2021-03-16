ae504aa084411baaf20e5aadaf2628fa7ace8ac2
Evgeniy <original.falin@yandex.ru>
2021-03-16 12:49:55 +0300

    Made <Sphere> and <Ball> classes

--- Ball.h
+++ Ball.h
@@ -9,7 +9,7 @@ public:
 	Ball(double intRadius, const std::string initName);
 	std::string getName();
 	void rename(const std::string newName);
-	void resetBall(double newRadius);
+	void resetBall(double newRadius, std::string newName);
 	void showInfo();
 private:
 	std::string name;
--- Sphere.h
+++ Sphere.h
@@ -10,3 +10,4 @@ public:
 protected:
 	double radius;
 };
+
